e6d1e9282afe26e85083520614989f938495582e
daviNGL <davi.ngl.100@gmail.com>
2022-09-04 14:40:36 -0300

    armazenamento caracteres

---
+++ tipo_dados.R
@@ -0,0 +1,30 @@
+
+# ARMAZENAMENTO NUMERICO
+
+salario <- 6500
+horas   <- 8*7*4
+
+sal_hor <- salario/horas
+sal_hor
+
+
+# CONVERTE PRA TIPO INTEIRO (APENAS TRUNCA NA PARTE INTEIRA)
+sal_hor_int <- as.integer(sal_hor)
+
+# ARREDONDAR O NUMERO ANTES DE CONVERTER PRA INTEIRO
+sal_hor_arr <- round(sal_hor)
+
+
+
+# ARMAZENAMENTO DE CARACTERES
+
+nome_1 <- "Gon Freecss"
+nome_2 <- "Killua Freecss"
+
+# CRIANDO VETOR COM OS NOMES
+nomes <- c(nome_1, nome_2)
+nomes
+
+
+nome_1 == nome_2
+nome_1 == nome_1

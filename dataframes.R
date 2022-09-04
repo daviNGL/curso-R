48e2a1e1082510a1a23a99c5134365aa380d6fb5
daviNGL <davi.ngl.100@gmail.com>
2022-09-04 16:09:33 -0300

    data frames

--- dataframes.R
+++ dataframes.R
@@ -16,3 +16,43 @@ df <- read.csv(file = "DataFrame.csv", sep = ";", header = TRUE)
 
 
 View(df)
+str(df)
+
+
+
+# SELECIONANDO VARIAVEIS DO DF:
+
+# CRIA UM DATA FRAME SO COM ESSA COLUNA
+df['NOME']
+
+# CRIA UMA VARIAVEL COM O MESMO TIPO DA COLUNA
+df$NOME
+
+
+# AJUSTANDO VARIAVEIS QUE NAO FICARAM CATEGORICAS
+df$CLASSE_NEN <- as.factor(df$CLASSE_NEN)
+df$ARCO_APARICAO <- as.factor(df$ARCO_APARICAO)
+
+str(df)
+summary(df)
+
+
+
+
+# EXCLUI UMA COLUNA
+bkp_idade <- df$IDADE
+df$IDADE = NULL
+
+
+# CRIANDO COLUNA
+df$idade <- bkp_idade
+
+
+
+df$protagonista <- FALSE
+df$protagonista[1:4] <- TRUE
+
+
+str(df)
+summary(df)
+

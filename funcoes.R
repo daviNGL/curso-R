2d577345e6fc8c407d58f402fd915f81ea2073ac
daviNGL <davi.ngl.100@gmail.com>
2022-09-04 14:18:52 -0300

    algumas funcoes nativas do R

---
+++ funcoes.R
@@ -0,0 +1,16 @@
+nome  <- "Gon"
+nome1 <- "Killua"
+nome2 <- "Leorio"
+nome3 <- "Kurapika"
+
+
+# FUNCAO 'C' CRIA UM VETOR COM OS VALORES INFORMADOS
+nome_completo <- c(nome, nome1, nome2, nome3)
+
+numeros_pares <- c(2,4,6,8,10,12,14,16,18,20)
+
+nome_completo
+numeros_pares
+
+
+

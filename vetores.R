8d2583190b6d54bd463ba732217668639f8572b2
daviNGL <davi.ngl.100@gmail.com>
2022-09-04 15:16:01 -0300

    lists

--- vetores.R
+++ vetores.R
@@ -1,8 +1,35 @@
 # VETOR -> quando tem uma sequencia de dados de apenas 1 tipo
 
-nomes <- c("Gon", "Killua", "Hisoka", "Pariston", as.integer(1))
+nomes <- c("Gon", "Killua", "Hisoka", "Pariston")
 
 summary(nomes)
 mode(nomes)
 class(nomes)
 
+
+
+# LISTAS -> UM VECTOR, EM QUE OS DADOS NAO SAO TODOS DO MESMO TIPO
+
+
+# SE UM DOS VALORES FOR CHAR, TODO O VETOR VIRA CHAR
+
+b <- c(1, 2, 3, "4", 5)
+summary(b)
+is.vector(b)
+
+
+# pra criar a list:
+
+b <- list(10, "20", 8)
+summary(b)
+is.vector(b)
+is.list(b)
+
+
+# LISTA COM UMA LISTA DENTRO
+
+lista_testes <- list(c(1,2,3), "20", 10)
+
+
+# ACESSAR UM VALOR DA LIST
+lista_testes[[1]][1]
